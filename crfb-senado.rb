c4d119d86770132c5ca2ddfd47c098576a191f65
Alexandre Magno <alexandre.mbm@gmail.com>
2014-12-08 04:12:54 -0300

    crfb-senado.rb parses the line command
    Now the script is distributed under Apache License 2.0

--- crfb-senado.rb
+++ crfb-senado.rb
@@ -1,23 +1,22 @@
 =begin
-Copyright (c) 2014 Alexandre Magno <alexandre.mbm@gmail.com>
+crfb-senado.rb is a Ruby script that downloads constitutional text from the
+Federal Senate of Brazil's website and convert them to Markdown format.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+crfb-senado.rb was the simple con1988.rb (of same author) under a MIT license
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+Copyright 2014 Alexandre Magno <alexandre.mbm@gmail.com>
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 =end
 
 # sudo apt-get install ruby-nokogiri
@@ -75,27 +74,123 @@ class Getter
 end
 
 require 'optparse'
+require 'ostruct'
 
-options = {}
+options = OpenStruct.new
+options.text = nil
+options.article = nil
+options.link = false
+options.notes = false
+options.cache = false
+options.amendment = nil
+
+=begin
+$ crfb-senado.rb -t 1988 -a 5          # salva o artigo 5º do texto de 1988
+$ crfb-senado.rb -t 2014 -a 5          # salva o artigo 5º do texto de 2014
+$ crfb-senado.rb -t 2014 -a 5 --notes  # salva o artigo 5º do texto de 2014 com notas de rodapé
+$ crfb-senado.rb --cache               # faz um cache que possibilita o comando -e
+$ crfb-senado.rb -e 25                 # salva arquivo da emenda 25
+$ crfb-senado.rb -e 25 --link          # atualiza artigos para ter ligações internas
+=end
+
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: ruby con1988.rb ARTICLE"
+  opts.banner  = "Usage: #{opts.program_name} INTEGER"
+  opts.separator "       #{opts.program_name} -t INTEGER -a INTEGER [--notes]"
+  opts.separator "       #{opts.program_name} -e INTEGER [--link]"
+  opts.separator "       #{opts.program_name} --cache"
+  opts.separator ""
+  opts.separator "   If using the unique parameter, it downloads article of 1988."
+  opts.separator ""
+  opts.on("-t INTEGER", Integer, "Year of the text: 1988 or 2014") do |year|
+    options.text = year
+  end
+  opts.on("-a INTEGER", Integer, "Number of the article") do |article|
+    options.article = article
+  end
+  opts.on("-e INTEGER", Integer, "Number of the amendment") do |e|
+    options.amendment = e
+  end
+  opts.on("--link", "To link internally in the articles") do
+    options.link = true
+  end
+  opts.on("--notes", "To create with footnotes") do
+    options.notes = true
+  end
+  opts.on("--cache", "To prepare cache to commands \"-e\"") do
+    options.cache = true
+  end
   opts.on_tail("-h", "--help", "Show this message") do
     puts opts
     exit
   end
 end
 
-optparse.parse!
-
-if ARGV.empty?
-  puts optparse
-  exit(-1)
+begin
+  optparse.parse!
+rescue OptionParser::InvalidOption => msg
+  puts msg
+  exit(-2)
+rescue OptionParser::InvalidArgument => msg
+  puts msg
+  exit(-2)
+rescue OptionParser::MissingArgument => msg
+  puts msg
+  exit(-2)
 end
 
-article = ARGV.pop
-raise "Need to specify a article to process" unless article
+if ARGV.empty?
+  if options.cache
+    if options.text or options.article or options.notes or options.amendment or options.link
+      puts "error: \"--cache\" was used together with others paramters"
+      exit(-1)
+    else
+      # crfb-senado.rb --cache
+      puts "cache is a functionality not implemented"  # TODO
+      exit
+    end
+  elsif options.amendment
+    if options.text or options.article or options.cache or options.notes
+      puts "error: \"-e INTEGER\" only can to be combined with \"--link\""
+      exit(-1)
+    elsif options.link
+      # crfb-senado.rb -e 25 --link
+      puts "amendment+link is a functionality not implemented"  # TODO
+      exit
+    else
+      # crfb-senado.rb -e 25
+      puts "amendment is a functionality not implemented"  # TODO
+      exit
+    end
+  elsif (options.text and !options.article) or (options.article and !options.text)
+      puts "error: \"-t INTEGER\" or \"-a INTEGER\" is missing"
+      exit(-1)
+  elsif options.text and options.article
+    if options.amendment or options.cache or options.link
+      puts "error: \"-t INTEGER -a INTEGER\" only can to be combined with \"--notes\""
+      exit(-1)
+    elsif options.notes
+      # crfb-senado.rb -t 1988 -a 5 --notes
+      # crfb-senado.rb -t 2014 -a 5 --notes
+      puts "(text/article)+notes is a functionality not implemented"  # TODO
+      exit
+    else
+      # crfb-senado.rb -t 1988 -a 5
+      # crfb-senado.rb -t 2014 -a 5
+      puts "text/article is a functionality not implemented"  # TODO
+      exit
+    end
+  else
+    puts optparse
+    exit(-1)
+  end
+end
 
-collector = Getter::new
-str = collector.con1988(article)
-
-File.write('art_' + article + '.md', str)
+begin
+  article = Integer(ARGV.pop)
+  raise "Need to specify a article to process" unless article  # called never
+  collector = Getter::new
+  str = collector.con1988(article)
+  File.write('art_' + article.to_s + '.md', str)
+rescue ArgumentError => msg
+  puts "Unique parameter needs to be an integer"
+end
